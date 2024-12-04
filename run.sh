16b4515aabe39702aba26e0330014caabf03c9b3
cs-mshr <cshekharmshr2407@gmail.com>
2024-12-04 11:35:44 +0530

    add DERIBIT get_order_book API

--- run.sh
+++ run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+set -e
+
+# Clone and build Crow if not already done
 if [ ! -d "dependencies/Crow/build" ]; then
     git clone https://github.com/CrowCpp/Crow.git dependencies/Crow
     mkdir -p dependencies/Crow/build
-    cd dependencies/Crow/build
+    pushd dependencies/Crow/build
     cmake .. -DCROW_BUILD_EXAMPLES=OFF -DCROW_BUILD_TESTS=OFF
     make install
-    cd ../../../
+    popd
 fi
 
-rm -rf build
-mkdir build
-cd build
+# Create build directory if it doesn't exist
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+# Build and run the project
+pushd build
 cmake ..
 make
 ./GoQuant
-
+popd
